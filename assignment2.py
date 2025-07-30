3abc05e4812e6470b19b204fa93c326e898dcf6b
Prerana610 <preranafuke610@gmail.com>
2025-07-30 17:58:35 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,12 @@
+'''
+user=int(input("Enter a number: "))
+
+if user/2==0:
+    print(user,'is even number')
+else:
+    print(user,'is odd number')
+'''
+total_sum=0
+for i in range(1,51):
+    total_sum += i
+print("the sum of numbers from 1 to 50 is: ",total_sum)
